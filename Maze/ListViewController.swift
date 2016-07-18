934c1e18158af173eea3394aeb380e477d8a2564
iwamotokasumi <kpe.359@icloud.com>
2016-07-18 15:46:17 +0900

    ファイル合体

--- Maze/ListViewController.swift
+++ Maze/ListViewController.swift
@@ -8,28 +8,67 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
-
+class ListViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    var manager: NSFileManager!
+    var fullPath: String!
+    var paths: Array<String>!
+    @IBOutlet weak var myTableView: UITableView!
+    @IBAction func tapBack(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        if paths == nil {
+            return 1
+        }
+        return paths.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        cell.textLabel?.text = paths[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let storyboard = UIStoryboard(name:"Main",bundle:nil)
+        let controller: SelectViewController = storyboard.instantiateViewControllerWithIdentifier("SelectViewController") as! SelectViewController
+        controller.path = paths[indexPath.row]
+        self.presentViewController(controller, animated: true, completion: nil)
+    }
+    
+    func refreshPaths() {
+        paths = manager.subpathsAtPath(fullPath)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        refreshPaths()
+        self.myTableView.reloadData()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        manager = NSFileManager.defaultManager()
+        fullPath = NSHomeDirectory() + "/Documents"
 
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- Maze/SelectViewController.swift
+++ Maze/SelectViewController.swift
@@ -9,6 +9,102 @@
 import UIKit
 
 class SelectViewController: UIViewController {
+    var path: String!     // this value should be set from the outer
+    var fullPath: String!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextView: UITextView!
+    
+    @IBAction func tapBack(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    @IBAction func tapLoad(sender: AnyObject) {
+        if let txt = myTextView.text {
+            let a:[String] = txt.componentsSeparatedByString(" ")
+            if a.count > 2 {
+                let rows = Int(a[0])
+                let cols = Int(a[1])
+                if (rows != nil) && (cols != nil) {
+                    var map = Array<Int>()
+                    for s in a {
+                        if let n = Int(s) {
+                            map.append(n)
+                        } else {
+                            return
+                        }
+                    }
+                    // success
+                    let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+                    appDelegate.rows = rows
+                    appDelegate.cols = cols
+                    appDelegate.map = map
+                    print("\(rows) \(cols) \(map.count)")
+                    dismissViewControllerAnimated(true, completion: nil)
+                }
+            }
+        }
+        
+    }
+    @IBAction func tapRemove(sender: AnyObject) {
+        do {
+            try NSFileManager.defaultManager().removeItemAtPath(fullPath)
+            dismissViewControllerAnimated(true, completion: nil)
+        } catch let error as NSError {
+            let alert: UIAlertController = UIAlertController(title:"Selected File",
+                                                             message: "error occurred: "+String(error),
+                                                             preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title:"Cancel",style:UIAlertActionStyle.Cancel,handler:nil))
+            presentViewController(alert,animated:true, completion:nil)
+        }
+    }
+    func fileContents() {
+        let manager:NSFileManager = NSFileManager.defaultManager()
+        var isDir: ObjCBool = false
+        let flag = manager.fileExistsAtPath(fullPath, isDirectory:&isDir)
+        if flag && Bool(isDir) {
+            myTextView.text = "[[Directory]]"
+        } else if flag {
+            if fullPath.hasSuffix(".txt") {
+                do {
+                    myTextView.text = try NSString(contentsOfFile: fullPath, encoding: NSUTF8StringEncoding) as String
+                } catch let error as NSError {
+                    let alert: UIAlertController = UIAlertController(title:"Selected File",
+                                                                     message: "cannot read .txt file: "+String(error),
+                                                                     preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title:"Cancel",style:UIAlertActionStyle.Cancel,handler:nil))
+                    presentViewController(alert,animated:true, completion:nil)
+                    
+                }
+            } else {
+                myTextView.text = "[[not directory, but has no \".txt\" suffix]]"
+            }
+        } else {
+            let alert: UIAlertController = UIAlertController(title:"Selected File",
+                                                             message: "No such file exists",
+                                                             preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title:"Cancel",style:UIAlertActionStyle.Cancel,handler:nil))
+            presentViewController(alert,animated:true, completion:nil)
+        }
+    }
+    
+    func setup() {
+        if path == nil {
+            let alert: UIAlertController = UIAlertController(title:"Selected File",
+                                                             message: "path is nil: ",
+                                                             preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title:"Cancel",style:UIAlertActionStyle.Cancel,handler:nil))
+            presentViewController(alert,animated:true, completion:nil)
+            path = ""
+        }
+        fullPath = NSHomeDirectory() + "/Documents/" + path
+        myLabel.text = path
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        setup()
+        fileContents()
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
